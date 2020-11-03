5949b86e423ed2653a25ce8a280ab0392ab4bf1a
Vika Svetlova <viktoria.svetlova@oxagile.com>
2020-11-03 16:25:47 +0300

    asdfg

--- copy.bat
+++ copy.bat
@@ -1,3 +1,5 @@
+RD /S /Q "Content\fonts"
 del /S /Q "Content\m-style.css"
 
-xcopy "project-help\from\style-test-mobile.css" "Content\" 
+xcopy "project-help\from\fonts" "Content\fonts\" /Y
+xcopy "project-help\from\style-test-mobile.css" "Content\" /Y
