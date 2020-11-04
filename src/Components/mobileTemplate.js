66967e9238f3c1ff849f73af9572966f58661c26
Vika Svetlova <viktoria.svetlova@oxagile.com>
2020-11-04 13:10:28 +0300

    sdf

--- src/Components/mobileTemplate.js
+++ src/Components/mobileTemplate.js
@@ -2,7 +2,7 @@ export const mobileIndex = `<!doctype html>
 <html>
     <head>
         <title>test</title>
-    <link href="./project-help/from/style-test-mobile.css" rel="stylesheet">
+    <link href="./Content/style-test-mobile.css" rel="stylesheet">
     </head>
     <body>
     <p>Mobile</p></body></html>`;
