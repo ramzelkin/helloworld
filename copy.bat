7aaf92588b14d2be1acdbe8b9e4baa86e118c198
Vika Svetlova <viktoria.svetlova@oxagile.com>
2020-11-05 20:26:04 +0300

    add picture

--- copy.bat
+++ copy.bat
@@ -2,4 +2,4 @@ RD /S /Q "Content\fonts"
 del /S /Q "Content\m-style.css"
 
 xcopy "project-help\from\fonts" "Content\fonts\" /Y
-xcopy "project-help\from\style-test-mobile.css" "Content\" /Y
+xcopy "project-help\from\style-test-mobile.css" "Content\css" /Y
