fa706d06df87d67b1ae2729c79ca7b13068dd939
Vika Svetlova <ramzelkin@yandex.ru>
2021-02-01 16:15:34 +0300

    asdv

--- src/Components/mobileTemplate.js
+++ src/Components/mobileTemplate.js
@@ -5,4 +5,40 @@ export const mobileIndex = `<!doctype html>
     <link href="./Content/style-test-mobile.css" rel="stylesheet">
     </head>
     <body>
+    <nav id="mobile_menu" class=" navbar navbar-expand-lg navbar-dark fixed-top bg-secondary">
+    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#applications" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" >Tuuuuu</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#profile-actions" aria-controls="navbarCollapse" aria-expanded="true" aria-label="Toggle navigation">
+        <span></span>
+    </button>
+
+    <div class="navbar-collapse collapse" id="applications" style="">
+
+        <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" >
+                        <span >Absence</span>
+                    </a>
+                </li>
+                
+                <li class="nav-item ">
+                    <a class="nav-link" >
+                            <span>Test 2</span>
+                    </a>
+                </li>
+        </ul>
+    </div>
+
+    <div class="navbar-collapse collapse" id="profile-actions" style="">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item nav-link-info">
+                <a href="#" class="nav-link e">
+                    <span>User</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
     <p>Mobile</p></body></html>`; 
