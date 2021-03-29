0c0d62945fcdd45f96d5edd802fd143fe98f8d11
Vika <Vika@DESKTOP-H9VHLJU>
2021-03-29 14:06:03 +0300

    sd

--- Build.sh
+++ Build.sh
@@ -3,6 +3,8 @@ rm -f Content/css/style-test-mobile.css
 cp -R -f project-help/from/fonts Content
 cp -f project-help/from/style-test-mobile.css Content/css
 npm run pages-build
-mkdir .public
-cp -r * .public
-mv .public public
+mkdir public
+cp -r * public
+# mkdir .public
+# cp -r * .public
+# mv .public public
