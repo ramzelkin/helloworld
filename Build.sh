5f6e2adf351b9ed710ba7836e3ebda185c697525
Vika <Vika@DESKTOP-H9VHLJU>
2021-03-29 11:35:47 +0300

    dsfd

---
+++ Build.sh
@@ -0,0 +1,8 @@
+rm -rf Content/fonts
+rm -f Content/css/style-test-mobile.css
+cp -R -f project-help/from/fonts Content
+cp -f project-help/from/style-test-mobile.css Content/css
+npm run pages-build
+mkdir .public
+cp -r * .public
+mv .public public
