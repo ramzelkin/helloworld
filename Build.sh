cf6237ee8023a263c5a9ee8c34718714e576ce14
Vika <Vika@DESKTOP-H9VHLJU>
2021-04-16 15:33:25 +0300

    asdd

--- Build.sh
+++ Build.sh
@@ -5,5 +5,5 @@ cp -f project-help/from/style-test-mobile.css Content/css
 rm -f Content/css/style.css
 cp -f project-help/style.css Content/css
 rm -rf Content/css/from
-cp -R -f project-help/from/fonts Content/css
+cp -R -f project-help/from/fonts Content/css/from/fonts
 npm run pages-build
