69ef8c38c0a172a03f4d538047d56e7eb15993f8
Vika <Vika@DESKTOP-H9VHLJU>
2021-04-16 14:16:48 +0300

    wdef

--- Build.sh
+++ Build.sh
@@ -3,6 +3,3 @@ rm -f Content/css/style-test-mobile.css
 cp -R -f project-help/from/fonts Content
 cp -f project-help/from/style-test-mobile.css Content/css
 npm run pages-build
-mkdir .public
-cp -r * .public
-mv .public public
