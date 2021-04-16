2cad22fcf6d90f13947e734f9280eee378ea4f86
Vika <Vika@DESKTOP-H9VHLJU>
2021-04-16 14:20:25 +0300

    dasf

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
@@ -31,7 +30,6 @@ module.exports = {
         ],
     },
     plugins: [
-        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             template: './test.html'
         }),
