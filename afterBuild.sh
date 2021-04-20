f42650e0a059e9477927367ff1f4e64164e0d4c2
Vika <Vika@DESKTOP-H9VHLJU>
2021-04-20 16:59:57 +0300

    sdsf

--- afterBuild.sh
+++ afterBuild.sh
@@ -1,3 +1,5 @@
+# Move all files from the root directory of the project to the public/ directory
+# The .public workaround is so cp doesn’t also copy public/ to itself in an infinite loop
 mkdir .public
 cp -a dist/. .public
 mkdir .public/Content
