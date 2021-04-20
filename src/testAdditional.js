2e4226f8770d2d7c2eb1d8f21d58d741109ef779
Vika <Vika@DESKTOP-H9VHLJU>
2021-04-20 13:22:10 +0300

    asdf

--- src/testAdditional.js
+++ src/testAdditional.js
@@ -1,8 +1,7 @@
 function showGood(){
-    var targetButton = document.getElementById('forMobile');
-    targetButton.addEventListener("click", function(){
+
          document.getElementsByTagName('body')[0].style.backgroundColor = "lightblue";
-    });
+    
 }
 
 showGood();
