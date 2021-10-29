d19d0fa90cd2116ec924eee369930423f93d60a2
Vika <Vika@DESKTOP-H9VHLJU>
2021-10-29 14:15:00 +0300

    dafsd

--- saveHash.js
+++ saveHash.js
@@ -1,6 +1,9 @@
 const distFolder = './dist/';
 const fs = require('fs');
 
-
-
-
+fs.readdir(distFolder, (err, files) => {
+    files.filter(file => {
+        console.log(files.includes('mian'));
+        return files.includes('mian');
+    })
+});
