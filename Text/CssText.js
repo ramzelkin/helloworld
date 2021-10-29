f4c3cd2dfbbf1ee6576b90b3d8394c92bb05ed21
Vika <Vika@DESKTOP-H9VHLJU>
2021-10-29 13:27:31 +0300

    hjk

--- Text/CssText.js
+++ Text/CssText.js
@@ -3,6 +3,92 @@ export let newTextCss = `This is css filehtml {
 }
 
 body {
+	background: #c58729;
+}
+
+p {
+	color: black;
+}
+
+@font-face {
+	font-family: ZillaSlabHighlight;
+	src: url("./from/fonts/ZillaSlabHighlight-Regular.ttf");
+}
+@font-face {
+	font-family: Itim;
+	src: url("./from/fonts/Itim-Regular.ttf");
+}
+@font-face {
+	font-family: Pacifico;
+	src: url("./from/fonts/Pacifico-Regular.ttf");
+}
+
+P {
+ font-family: Pacifico;
+}
+
+p {
+	font-weight: normal;
+}
+
+#showMobile {
+	border: 3px solid brown;
+	background: orange;
+	color: yellow;
+	font-family: Itim;
+	outline: 1px solid #666;
+}
+ img {
+	 border: 5px solid black;
+ }
+And also some textThis is css filehtml {
+	height: 100%;
+}
+
+body {
+	background: #c58729;
+}
+
+p {
+	color: black;
+}
+
+@font-face {
+	font-family: ZillaSlabHighlight;
+	src: url("./from/fonts/ZillaSlabHighlight-Regular.ttf");
+}
+@font-face {
+	font-family: Itim;
+	src: url("./from/fonts/Itim-Regular.ttf");
+}
+@font-face {
+	font-family: Pacifico;
+	src: url("./from/fonts/Pacifico-Regular.ttf");
+}
+
+P {
+ font-family: Pacifico;
+}
+
+p {
+	font-weight: normal;
+}
+
+#showMobile {
+	border: 3px solid brown;
+	background: orange;
+	color: yellow;
+	font-family: Itim;
+	outline: 1px solid #666;
+}
+ img {
+	 border: 5px solid black;
+ }
+And also some textThis is css filehtml {
+	height: 100%;
+}
+
+body {
 	background: #ffd79f;
 }
 
